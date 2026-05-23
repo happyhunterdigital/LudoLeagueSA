a9c797364bbeb13eb588c363f1b82ea34dbe1c40
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-05-23 14:38:08 +0200

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import { Page } from '../App';
 import { LandingHero } from '../components/features/LandingHero';
-import { ValuePropsAndPrograms } from '../components/features/ValuePropsAndPrograms';
+import { LandingCarousel } from '../components/features/LandingCarousel';
+import { LandingFeatures } from '../components/features/LandingFeatures';
+import { LandingGallery } from '../components/features/LandingGallery';
+import { LandingSystems } from '../components/features/LandingSystems';
+import { LandingMediaGrid } from '../components/features/LandingMediaGrid';
+import { LandingCTA } from '../components/features/LandingCTA';
 import { CommunityFund } from '../components/features/CommunityFund';
-import { StoryAndSocialProof } from '../components/features/StoryAndSocialProof';
 
 export const Landing = ({ setActivePage }: { setActivePage: (p: Page) => void }) => {
   return (
     <div className="flex flex-col w-full">
-      {/* 1. Hero Section */}
-      <LandingHero setActivePage={setActivePage} />
+      {/* 2. Hero Section */}
+      <LandingHero scrollToSection={(id) => setActivePage('Tournaments')} />
       
-      {/* 2. Value Props & 3. Programs */}
-      <ValuePropsAndPrograms setActivePage={setActivePage} />
-      
-      {/* 4. Contribution/Donation Section */}
+      {/* 3. News Ticker Carousel */}
+      <LandingCarousel />
+
+      {/* 4. Three-Column Feature Highlights */}
+      <LandingFeatures />
+
+      {/* 5. Custom Board Selection Gallery */}
+      <LandingGallery />
+
+      {/* 6 & 7. Core Systems & Sporting Structure */}
+      <LandingSystems />
+
+      {/* 8. Media Content Grid */}
+      <LandingMediaGrid />
+
+      {/* 4. Donation/Contribution Gateway */}
       <div id="donation-section">
         <CommunityFund />
       </div>
-      
-      {/* 5. About, 6. Social Proof, 7. Founder, 8. CTA */}
-      <StoryAndSocialProof setActivePage={setActivePage} />
+
+      {/* 9. Secondary CTA / Community Links */}
+      <LandingCTA />
     </div>
   );
 };
