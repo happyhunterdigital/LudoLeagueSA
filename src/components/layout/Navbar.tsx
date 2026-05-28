e84fad784e495fe7460b04163cb09968de4b1f70
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-05-28 17:34:36 +0200

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion } from 'motion/react';
+import { motion, AnimatePresence } from 'motion/react';
 import { Heart, ShoppingCart, ShoppingBag, Menu, X, ChevronDown } from 'lucide-react';
 import { Page } from '../../App';
 
