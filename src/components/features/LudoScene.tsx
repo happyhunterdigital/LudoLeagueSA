af4611cea36df252832a0efc355558818ca968dc
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-06-03 02:57:59 +0200

    Update LudoScene.tsx

--- src/components/features/LudoScene.tsx
+++ src/components/features/LudoScene.tsx
@@ -13,27 +13,36 @@ interface LudoSceneProps {
 export const LudoScene: React.FC<LudoSceneProps> = ({ selectedId, setSelectedId, addToCart, cart }) => {
   const { viewport } = useThree();
 
-  const boards = [
+  const products = [
     { 
-      id: 'board-purple', 
-      name: 'Royal Purple', 
-      color: '#4B0082', 
-      xPos: -2.8,
-      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458042/Final_Purple_Board_hohd9k.png'
+      id: 'board-purple', name: 'Royal Purple', color: '#4B0082', xPos: -5,
+      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458042/Final_Purple_Board_hohd9k.png',
+      price: 1200, isBoard: true
     },
     { 
-      id: 'board-original', 
-      name: 'Classic Teal', 
-      color: '#008080', 
-      xPos: 0,
-      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458042/Final_Original_Board_m6uyqi.png'
+      id: 'board-original', name: 'Classic Teal', color: '#008080', xPos: -2.5,
+      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458042/Final_Original_Board_m6uyqi.png',
+      price: 1200, isBoard: true
     },
     { 
-      id: 'board-orange', 
-      name: 'Amber Orange', 
-      color: '#FF8C00', 
-      xPos: 2.8,
-      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458041/Final_Orange_Board_h3mopp.png'
+      id: 'board-black', name: 'Obsidian Black', color: '#111111', xPos: 0,
+      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458041/Final_Black_Board_aq9yjo.png',
+      price: 1200, isBoard: true
+    },
+    { 
+      id: 'board-blue', name: 'Electric Blue', color: '#1E90FF', xPos: 2.5,
+      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458041/Final_Blue_Board_mvqu4j.png',
+      price: 1200, isBoard: true
+    },
+    { 
+      id: 'board-orange', name: 'Amber Orange', color: '#FF8C00', xPos: 5,
+      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458041/Final_Orange_Board_h3mopp.png',
+      price: 1200, isBoard: true
+    },
+    { 
+      id: 'tokens-dice', name: 'Token & Dice Set', color: '#E8A020', xPos: 7.2,
+      imgUrl: 'https://res.cloudinary.com/dkyg07qvv/image/upload/v1779458041/Final_Tokens_and_Dice_gk3mbd.png',
+      price: 200, isBoard: false
     }
   ];
 
@@ -43,25 +52,32 @@ export const LudoScene: React.FC<LudoSceneProps> = ({ selectedId, setSelectedId,
       <ambientLight intensity={0.15} />
       <directionalLight position={[0, 8, 4]} intensity={0.6} castShadow />
 
-      <spotLight position={[-2.8, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.8} castShadow />
-      <spotLight position={[0, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.8} castShadow />
-      <spotLight position={[2.8, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.8} castShadow />
+      {/* Structured spotlights with warm highlights */}
+      <spotLight position={[-5, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.5} castShadow />
+      <spotLight position={[-2.5, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.5} castShadow />
+      <spotLight position={[0, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.5} castShadow />
+      <spotLight position={[2.5, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.5} castShadow />
+      <spotLight position={[5, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.5} castShadow />
+      <spotLight position={[7.2, 4.5, 2.5]} angle={0.25} penumbra={1} intensity={1.5} castShadow />
 
-      {boards.map(board => (
+      {products.map(item => (
         <LudoBoardModel
-          key={board.id}
-          id={board.id}
-          name={board.name}
-          color={board.color}
-          imgUrl={board.imgUrl}
-          position={[board.xPos, 0, 0]}
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          color={item.color}
+          imgUrl={item.imgUrl}
+          price={item.price}
+          isBoard={item.isBoard}
+          position={[item.xPos, 0, 0]}
           selectedId={selectedId}
           setSelectedId={setSelectedId}
           addToCart={addToCart}
-          isAdded={cart.includes(board.id)}
+          isAdded={cart.includes(item.id)}
         />
       ))}
 
+      {/* High-performance glossy floor plane reflecting your 3D boards */}
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.3, 0]}>
         <planeGeometry args={[50, 50]} />
         <MeshReflectorMaterial
