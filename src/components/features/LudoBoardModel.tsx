f87dd62b0fdba1e3885d6e131c0435df5e305781
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-06-10 01:17:53 +0200

    Refactor LudoScene to LudoBoardModel with rotation

--- src/components/features/LudoBoardModel.tsx
+++ src/components/features/LudoBoardModel.tsx
@@ -1,35 +1,27 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Stage, MeshReflectorMaterial } from '@react-three/drei';
-import { LudoBoardModel } from './LudoBoardModel';
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import * as THREE from 'three';
 
-export const LudoScene = () => {
+export const LudoBoardModel = (props: any) => {
+  const group = useRef<THREE.Group>(null);
+  
+  useFrame((state) => {
+    if (group.current) {
+      group.current.rotation.y = state.clock.getElapsedTime() * 0.15;
+    }
+  });
+
   return (
-    <div className="w-full h-full min-h-[400px]">
-      <Canvas dpr={[1, 2]} camera={{ position: [0, 0, 150], fov: 40 }}>
-        <Suspense fallback={null}>
-          <Stage environment="city" intensity={0.65} contactShadow={false}>
-            <LudoBoardModel />
-          </Stage>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
-            <planeGeometry args={[170, 170]} />
-            <MeshReflectorMaterial
-              blur={[300, 100]}
-              resolution={2048}
-              mixBlur={1}
-              mixStrength={40}
-              roughness={1}
-              depthScale={1.2}
-              minDepthThreshold={0.4}
-              maxDepthThreshold={1.4}
-              color="#101010"
-              metalness={0.5}
-              mirror={1}
-            />
-          </mesh>
-        </Suspense>
-        <OrbitControls enableZoom={false} enablePan={false} minPolarAngle={Math.PI / 3} maxPolarAngle={Math.PI / 2} />
-      </Canvas>
-    </div>
+    <group
+      ref={group}
+      position={[0, 0, 0]}
+      onClick={() => {}}
+      {...props}
+    >
+      <mesh castShadow receiveShadow>
+        <boxGeometry args={[4, 0.2, 4]} />
+        <meshStandardMaterial color="#0c4a60" roughness={0.3} metalness={0.8} />
+      </mesh>
+    </group>
   );
 };
