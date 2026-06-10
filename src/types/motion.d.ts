31509370431b1a1c4d92f3fe565e21290e5f21cc
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-06-10 12:13:09 +0200

    Update motion.d.ts

--- src/types/motion.d.ts
+++ src/types/motion.d.ts
@@ -1,4 +1,5 @@
 declare module 'motion/react' {
-  import { motion, AnimatePresence, useSpring, useScroll } from 'framer-motion';
-  export { motion, AnimatePresence, useSpring, useScroll };
+  import { motion, AnimatePresence, useSpring, useScroll, useInView, useTransform, useAnimation, useAnimationControls, useIsPresent, useMotionValue } from 'framer-motion';
+  export { motion, AnimatePresence, useSpring, useScroll, useInView, useTransform, useAnimation, useAnimationControls, useIsPresent, useMotionValue };
+  export * from 'framer-motion';
 }
