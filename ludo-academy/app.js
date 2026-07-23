2ebf2f86caf1f8ff118a436815b655e6f07d7ce5
Thabo Leslie Motsumi <happyhunterdigital@gmail.com>
2026-07-23 13:24:28 +0200

    Update navigation to tournaments in App component

--- ludo-academy/app.js
+++ ludo-academy/app.js
@@ -1,22 +1,22 @@
-const { useState } = React;
-const { RegistrationForm, CookieConsent } = window.LudoForms;
-const { HeroSection, MarqueeSection } = window.LudoSections;
-const { AboutSection, ServicesSection } = window.LudoSections2;
-const { ProjectsSection, Footer } = window.LudoSections3;
+const { CookieConsent } = window.LudoForms || {};
+const { HeroSection, MarqueeSection } = window.LudoSections || {};
+const { AboutSection, ServicesSection } = window.LudoSections2 || {};
+const { ProjectsSection, Footer } = window.LudoSections3 || {};
+
+const navigateToTournaments = () => {
+  window.location.href = 'https://ludoleague.co.za/#tournaments';
+};
 
 const App = () => {
-  const [isFormOpen, setIsFormOpen] = useState(false);
-
   return (
     <div className="overflow-x-clip relative">
-      <HeroSection openForm={() => setIsFormOpen(true)} />
+      <HeroSection openForm={navigateToTournaments} />
       <MarqueeSection />
       <AboutSection />
       <ServicesSection />
       <ProjectsSection />
-      <Footer openForm={() => setIsFormOpen(true)} />
-      <RegistrationForm isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} />
-      <CookieConsent />
+      <Footer openForm={navigateToTournaments} />
+      {CookieConsent && <CookieConsent />}
     </div>
   );
 };
